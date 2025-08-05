2b08dbfc50cba4e0e4cff398a0dc3c4677ef45a6
Shivamt7 <shivam.s.mishra7@gmail.com>
2025-08-05 21:11:19 +0530

    update

--- src/components/ReusableComponentWithConditionalRendering/Cards.jsx
+++ src/components/ReusableComponentWithConditionalRendering/Cards.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Cards = ({ item }) => {
+
+  const [readmore, setReadMore] = useState(false)
+
   return (
-    <div className="w-52 h-80 bg-zinc-100 rounded-md overflow-hidden">
+    <div className="w-52 bg-zinc-100 rounded-md overflow-hidden">
       <div className="w-full h-32 bg-zinc-300">
         <img
           className="w-full h-full object-cover"
@@ -12,8 +15,9 @@ const Cards = ({ item }) => {
       </div>
       <div className="w-full px-3 py-4">
         <h2 className="font-semibold">{item.title}</h2>
-        <p className="text-xs mt-3">
-          {`${item.description.substring(0, 200)}...`}
+        <p className="text-xs mt-3 text-justify">
+          {readmore === false ? `${item.description.substring(0, 190)}...` : `${item.description}`}
+          <span className={`${readmore ? "text-blue-600" : "text-red-600"} cursor-pointer`} onClick={() => setReadMore(() => !readmore)}>{readmore ? "Show Less" : "Read More"}</span>
         </p>
       </div>
     </div>
--- src/components/ReusableComponentWithConditionalRendering/Page.jsx
+++ src/components/ReusableComponentWithConditionalRendering/Page.jsx
@@ -4,7 +4,7 @@ import { data } from "./Data";
 
 const Page = () => {
   return (
-    <div className="flex w-full h-screen justify-around gap-10 items-center bg-zinc-200 flex-wrap">
+    <div className="flex w-full h-screen justify-around gap-10 items-center bg-zinc-200 flex-wrap px-10">
       {data.map((item) => (
         <Cards key={item.title} item={item} />
       ))}
